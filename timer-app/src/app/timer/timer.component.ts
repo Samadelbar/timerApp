9b16a22c9c52ae3248163a2fbc3ae4b5b8667db9
Samaneh Delbari <sama.delbari400@gmail.com>
2022-06-08 02:12:51 +0430

    add com.ts

--- timer-app/src/app/timer/timer.component.ts
+++ timer-app/src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+  private timer: any;
+  private date= new Date();
+  public show: boolean= true;
+  public disabled: boolean = false;
+  public animate: boolean = false;
+  @ViewChild("idAudio") idAudio?: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  increment(type: "H" | "M" | "S"){
+    if(type === "H"){
+      if (this.hours >= 99) return;
+      this.hours += 1;
+    }
+    else if(type === "M"){
+      if(this.minutes >= 59) return;
+      this.minutes += 1;
+    }else{
+      if(this.seconds >= 59) return;
+      this.seconds += 1;
+    }
+  }
+
+  decrement(type: "H" | "M" | "S"){
+    if(type === "H"){
+      if (this.hours <= 0) return;
+      this.hours -= 1;
+    }
+    else if(type === "M"){
+      if(this.minutes <= 0) return;
+      this.minutes -= 1;
+    }else{
+      if(this.seconds <= 0) return;
+      this.seconds -= 1;
+    }
   }
 
 }
